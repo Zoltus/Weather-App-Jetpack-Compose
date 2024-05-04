a18372147adf5f4d84cd3d33bd1189752db5fa7e
Zoltus <zoltus@outlook.com>
2024-05-04 18:43:14 +0300

    feat: add background to details & daily components

--- app/src/main/java/fi/sulku/weatherapp/components/Daily.kt
+++ app/src/main/java/fi/sulku/weatherapp/components/Daily.kt
@@ -1,9 +1,27 @@
 package fi.sulku.weatherapp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Daily() {
-    Text("DAILY CHART")
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(
+                color = Color(0xFFBFDBFE),
+                shape = RoundedCornerShape(16.dp) // Rounded corners
+            )
+    ) {
+        Text("DAILY CHART")
+    }
 }
--- app/src/main/java/fi/sulku/weatherapp/components/Details.kt
+++ app/src/main/java/fi/sulku/weatherapp/components/Details.kt
@@ -1,38 +1,59 @@
 package fi.sulku.weatherapp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Details() {
-    Text("DETAILS")
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(8.dp)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(
+                color = Color(0xFFBFDBFE),
+                shape = RoundedCornerShape(16.dp) // Rounded corners
+            )
     ) {
-        item {
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-        }
-        item {
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
-            Text("DETAILS")
+        Text("DETAILS")
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            item {
+                Column {
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                }
+            }
+            item {
+                Column {
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                    Text("DETAILS")
+                }
+            }
         }
     }
 }
