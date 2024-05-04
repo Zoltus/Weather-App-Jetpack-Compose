0ceb4732a36921c37cc14470300da8bde1a642a1
Zoltus <zoltus@outlook.com>
2024-05-04 18:44:27 +0300

    feat: make hourly component fillMaxWidth

--- app/src/main/java/fi/sulku/weatherapp/components/Hourly.kt
+++ app/src/main/java/fi/sulku/weatherapp/components/Hourly.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -16,7 +17,7 @@ import androidx.compose.ui.unit.dp
 fun Hourly() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .padding(16.dp)
             .background(
                 color = Color(0xFFBFDBFE),
