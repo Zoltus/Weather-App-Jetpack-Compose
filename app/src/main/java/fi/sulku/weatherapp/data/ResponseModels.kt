d1df94a8bbe4e598efee1ad09821ed96cf2f324e
Zoltus <zoltus@outlook.com>
2024-05-04 22:53:17 +0300

    refactor: remove unused variable from weatherdata

--- app/src/main/java/fi/sulku/weatherapp/data/ResponseModels.kt
+++ app/src/main/java/fi/sulku/weatherapp/data/ResponseModels.kt
@@ -11,7 +11,6 @@ data class WeatherResponse(
 @Serializable
 data class Current(
     val time: String,
-    val interval: Int,
     val temperature_2m: Double,
     val relative_humidity_2m: Int,
     val apparent_temperature: Double,
