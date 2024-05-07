07cf017d2a8c916af181ce11813dd3ee8ddf0669
Zoltus <zoltus@outlook.com>
2024-05-07 11:53:31 +0300

    refactor: move permissiondialog to separate composable

--- app/src/main/java/fi/sulku/weatherapp/components/AskLocation.kt
+++ app/src/main/java/fi/sulku/weatherapp/components/AskLocation.kt
@@ -6,28 +6,23 @@ import android.net.Uri
 import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import fi.sulku.weatherapp.data.LocationViewModel
-import fi.sulku.weatherapp.data.WeatherViewModel
 
 @Composable
 fun AskLocation() {
-    val vm: WeatherViewModel = viewModel()
     val lv: LocationViewModel = viewModel()
     val location = lv.location.collectAsState()
-    val context = LocalContext.current
-    var showDialog by remember { mutableStateOf(false) }
+    val showDialog: MutableState<Boolean> = remember { mutableStateOf(false) }
 
     val permissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
@@ -37,10 +32,9 @@ fun AskLocation() {
             if (allPermissionsGranted) {
                 //if granted start location updates
                 lv.startLocationUpdates()
-                println("@@@@@@@@@@@")
             } else {
                 //if denied show dialog
-                showDialog = true
+                showDialog.value = true
             }
         }
     )
@@ -65,27 +59,33 @@ fun AskLocation() {
         Text("📍")
     }
 
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text("Permission required") },
-            text = { Text("To get your current location the app needs location permissions. Please grant the permissions or search by city name.") },
-            confirmButton = {
-                Button(onClick = {
-                    showDialog = false
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri: Uri = Uri.fromParts("package", context.packageName, null)
-                    intent.data = uri
-                    context.startActivity(intent)
-                }) {
-                    Text("Open Settings")
-                }
-            },
-            dismissButton = {
-                Button(onClick = { showDialog = false }) {
-                    Text("Dismiss")
-                }
-            }
-        )
+    if (showDialog.value) {
+        PermissionDialog(showDialog)
     }
+}
+
+@Composable
+fun PermissionDialog(showDialog : MutableState<Boolean>) {
+    val context = LocalContext.current
+    AlertDialog(
+        onDismissRequest = { showDialog.value = false },
+        title = { Text("Permission required") },
+        text = { Text("To get your current location the app needs location permissions. Please grant the permissions or search by city name.") },
+        confirmButton = {
+            Button(onClick = {
+                showDialog.value = false
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri: Uri = Uri.fromParts("package", context.packageName, null)
+                intent.data = uri
+                context.startActivity(intent)
+            }) {
+                Text("Open Settings")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { showDialog.value = false }) {
+                Text("Dismiss")
+            }
+        }
+    )
 }
