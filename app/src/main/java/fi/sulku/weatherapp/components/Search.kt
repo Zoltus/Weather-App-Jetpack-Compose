278ab286c7bec35217c2eb6051fca6810bf0df33
Zoltus <zoltus@outlook.com>
2024-05-08 14:21:48 +0300

    feat: add style to search section

--- app/src/main/java/fi/sulku/weatherapp/components/Search.kt
+++ app/src/main/java/fi/sulku/weatherapp/components/Search.kt
@@ -1,9 +1,11 @@
 package fi.sulku.weatherapp.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -18,7 +20,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import fi.sulku.weatherapp.data.WeatherApiService
 import fi.sulku.weatherapp.data.WeatherViewModel
 import kotlinx.coroutines.launch
 
@@ -31,20 +32,33 @@ fun Search() {
     var input by remember { mutableStateOf("") }
 
     Text(text = "Check Weather")
-    Row {
-        GpsLocButton()
-        Spacer(modifier = Modifier.width(8.dp))
-        TextField(modifier = Modifier.height(50.dp),
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp),
+    ) {
+        Row {
+            GpsLocButton()
+            Spacer(modifier = Modifier.width(8.dp))
+            TextField(modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth(),
+                shape = RoundedCornerShape(20),
+                value = input, onValueChange = { input = it },
+                placeholder = { Text("City name") }
+            )
+        }
+
+        Button(modifier = Modifier.fillMaxWidth()
+            .padding(top = 16.dp),
             shape = RoundedCornerShape(20),
-            value = input, onValueChange = { input = it },
-            placeholder = { Text("City name") }
-        )
+            onClick = {
+                scope.launch { vm.fetchWeather(input) }
+            }) {
+            Text("Get Weather")
+        }
     }
-    Button(modifier = Modifier.fillMaxWidth(),
-        onClick = {
-        scope.launch { vm.getWeather(input) }
-    }) {
-        Text("Get Weather")
-    }
+
 }
 
